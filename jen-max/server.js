49a3ffc7a2bdb8ffbad7231d5607cd3f1582b5d9
Max <gusstorm132@gmail.com>
2018-04-20 12:22:05 -0700

    updateRecord is functioning

--- jen-max/server.js
+++ jen-max/server.js
@@ -92,13 +92,30 @@ app.post('/articles', (request, response) => {
 
 app.put('/articles/:id', function (request, response) {
   client.query(
-    ``,
-    []
+    `UPDATE articles
+    SET
+    title=$1, category=$2, "publishedOn"=$3, body=$4
+    WHERE article_id=$5
+     `, [
+      request.body.title,
+      request.body.category,
+      request.body.publishedOn,
+      request.body.body,
+      request.params.id
+    ]
   )
     .then(() => {
       client.query(
-        ``,
-        []
+        `UPDATE authors
+        SET
+        author=$1,
+        "authorUrl"=$2
+        WHERE author_id=$3`,
+        [
+          request.body.author,
+          request.body.authorUrl,
+          request.body.author_id
+        ]
       )
     })
     .then(() => {
