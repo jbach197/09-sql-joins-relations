093bbf73d0a6d0d9470e66a0d8c6f594e6cdf1bb
Max <gusstorm132@gmail.com>
2018-04-20 11:53:23 -0700

    joined authors to articles for populating

--- jen-max/server.js
+++ jen-max/server.js
@@ -25,7 +25,7 @@ app.get('/new', (request, response) => {
 
 // REVIEW: These are routes for making API calls to enact CRUD operations on our database.
 app.get('/articles', (request, response) => {
-  client.query(`SELECT * FROM articles`)
+  client.query(`SELECT * FROM articles INNER JOIN authors ON articles.author_id = authors.author_id`)
     .then(result => {
       response.send(result.rows);
     })
